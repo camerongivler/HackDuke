9582c347b615b4fe0b14ecc64ac835eab173605e
Cameron Givler <cgiv812@gmail.com>
2013-11-17 11:11:44 -0500

    initial commit

---
+++ client/ufo.js
@@ -0,0 +1,251 @@
+    var strength = 0;
+    var maxHealth;
+    var currHealth = 10;
+    var velocity = 10;
+    var angle = 0;
+    var bulletAngle;
+    var aVel = 9;
+    var height = $(window).height() - 25;
+    var width = $(window).width() - 25;
+    var shipX = Math.floor(Math.random() * width); // Change to reflect ship size
+    var shipY = Math.floor(Math.random() * height); // Change to reflect ship size
+    var bulletPosX;
+    var bulletPosY;
+    var bulletVel = 50;
+    var playerName = prompt("What is your username?");
+    var interval;
+    var bulletFlag = false;
+    var ships = [];
+    var myNum;
+    var myShip;
+    var keys = [];
+    var myBullet;
+    var socket;
+    var events;
+
+    $(function () {
+        //socket connection
+        socket = io.connect();
+
+        socket.on('connect', function () {
+            socket.emit('size', {
+                height: height,
+                width: width
+            });
+            socket.emit('name', playerName);
+        });
+
+        socket.on('size', function (size) {
+            height = size.height;
+            width = size.width;
+            $('#view').height(height);
+            $('#view').width(width);
+        });
+
+        socket.on('ready', function (num) {
+            myNum = num;
+            init();
+        });
+
+        socket.on('names', function (names) { 
+            for (var k = ships.length; k<names.length; k++) {
+                if (k== myNum) continue;
+                $('#view').append('<div class="ship" id = "' + k + '">' + names[k] + '</div>');
+            }
+            ships = names;
+            $('#0').css({
+                'background-color':'green'
+            });
+            $('#1').css({
+                'background-color':'green'
+            });
+            $('#2').css({
+                'background-color':'orange'
+            });
+            $('#3').css({
+                'background-color':'red'
+            });
+        });
+
+        socket.on('positions', function (pos) {
+            updatePositions(pos);
+        });
+        
+        socket.on('leave', function(){
+            clearInterval(events);
+            myShip = null;
+            myBullet = null;
+            playerName = null;
+            velocity = null;
+            socket = null;
+            events = null;
+        });
+        
+        socket.on('stopBullet', function () {
+            clearInterval(interval);
+            myBullet.hide();
+            bulletPosX = -1;
+            bulletPosY = -1;
+            bulletFlag = false;
+        });
+        
+        socket.on('hit', wasAttacked);
+        
+        socket.on('remove', function (number) {
+            isRemoved(number);
+        });
+
+        setInterval(function () {
+            socket.emit('position', {
+                shipY: shipY,
+                shipX: shipX,
+                angle: angle,
+                bulletPosX: bulletPosX,
+                bulletPosY: bulletPosY
+            });
+        }, 50);
+
+        $(window).resize(function () {
+            if($(window).height() - 25 > height && $(window).width - 25 > width)
+                return;
+            height = $(window).height() - 25;
+            width = $(window).width() - 25;
+            socket.emit('size', {height: height, width: width});
+        });
+    });
+
+    // Initialization
+    var init = function () {
+        $('#view').append('<div class="ship" id="' + myNum + '"></div>');
+        for(var k = 0; k < 4; k++){
+            $('#view').append('<div class="bullet" id="b' + k + '"></div>');
+            $('#b' + k).hide();
+        }
+        myShip = $('#' + myNum);
+        myBullet = $('#b' + myNum);
+        myShip.text(playerName);
+        draw(myShip, shipX, shipY, angle);
+        events = setInterval(eventLoop, 50);
+
+        $(window).keydown(function (event) {
+            keys[event.which] = true;
+        }).keyup(function (event) {
+            delete keys[event.which];
+        });
+        draw(myShip, shipX, shipY, angle);
+    };
+
+
+    // Movement
+    var draw = function (ship, x, y, ang) {
+        ship.css({
+            top: y,
+            left: x
+        });
+        ship.css({
+            transform: 'rotate(' + ang + 'deg)'
+        });
+    };
+
+    var drawBullet = function () {
+        if (!bulletFlag) {
+            myBullet.show();
+            bulletFlag = true;
+            bulletAngle = angle;
+            bulletPosY = Math.round(shipY - 30 * Math.cos((bulletAngle / 360) * 2 * Math.PI) - 5);
+            bulletPosX = Math.round(shipX + 30 * Math.sin((bulletAngle / 360) * 2 * Math.PI) - 5);
+            myBullet.css({
+                top: bulletPosY,
+                left: bulletPosX
+            });
+            interval = setInterval(updateBullet, 50);
+        }
+    };
+
+    var updateBullet = function () {
+
+        bulletPosX += Math.round(bulletVel * Math.sin((bulletAngle / 360) * 2 * Math.PI));
+        bulletPosY -= Math.round(bulletVel * Math.cos((bulletAngle / 360) * 2 * Math.PI));
+
+        myBullet.css({
+            top: bulletPosY,
+            left: bulletPosX
+        });
+        if (bulletPosY < 0 || bulletPosY > height || bulletPosX < 0 || bulletPosX > width) {
+            clearInterval(interval);
+            myBullet.hide();
+            bulletPosX = -1;
+            bulletPosY = -1;
+            bulletFlag = false;
+        }
+    };
+    
+    var enemyBullet = function(bullet, posX, posY){
+        if(posX == -1 || posY == -1)
+            bullet.hide();
+        else{
+            bullet.show();
+            bullet.css({left: posX, top: posY});
+        }
+    };
+
+    var updatePositions = function (pos) {
+        for (var k = 0; k < pos.length; k++) {
+            if (k == myNum) continue;
+            draw($('#' + k), pos[k].shipX, pos[k].shipY, pos[k].angle);
+            enemyBullet($('#b' + k), pos[k].bulletPosX, pos[k].bulletPosY);
+        }
+    };
+
+    var eventLoop = function () {
+        if (keys[37]) {
+            angle -= aVel;
+            if (angle < 0) angle += 360;
+        }
+        if (keys[38]) {
+            shipY -= Math.round(Math.cos((angle / 360) * 2 * Math.PI) * velocity);
+            shipX += Math.round(Math.sin((angle / 360) * 2 * Math.PI) * velocity);
+
+        }
+        if (keys[39]) {
+            angle += aVel;
+            if (angle >= 360) angle -= 360;
+        }
+        if (keys[40]) {
+            shipY += Math.round(Math.cos((angle / 360) * 2 * Math.PI) * velocity);
+            shipX -= Math.round(Math.sin((angle / 360) * 2 * Math.PI) * velocity);
+        }
+        if (keys[32]) {
+            drawBullet();
+        }
+        if (shipX > width - 35) shipX = width - 35;
+        if (shipX < 35) shipX = 35;
+        if (shipY > height - 35) shipY = height - 35;
+        if (shipY < 35) shipY = 35;
+        draw(myShip, shipX, shipY, angle);
+    };
+    
+    var wasAttacked = function()
+    {
+        currHealth -= 1 - strength;
+        if(currHealth < 4)
+            $('#health').css('background-color', 'red');
+        $('#health').css({width: currHealth*10 + "px"});
+        if(currHealth <= 0)
+        {
+            socket.emit('died');
+            myShip.remove();
+            myBullet.remove();
+            clearInterval(interval);
+            bulletPosX = -1;
+            bulletPosY = -1;
+            bulletFlag = true;
+            clearInterval(events);
+        }
+    }
+    
+    var isRemoved = function(number)
+    {
+        $('#' + number).remove();
+        $('#b' + number).remove();
+    }
---
+++ server.js
@@ -0,0 +1,102 @@
+var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+var socketio = require('socket.io');
+var express = require('express');
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+var clients = [];
+var positions = [];
+var names = [];
+var width = 10000000;
+var height = 10000000;
+
+router.use(express.static(path.resolve(__dirname, 'client')))
+
+io.set('log level', 1);
+io.on('connection', function (socket) {
+    if(clients.length >=4){
+        socket.emit('leave');
+        return;
+    }
+    clients.push(socket);
+    positions.push({
+        shipX: 0,
+        shipY: 0,
+        angle: 0
+    });
+    socket.set('num', clients.indexOf(socket));
+
+    socket.on('name', function (name) {
+        socket.set('name', name, function () {
+            console.log('name: ' + name);
+            socket.get('num', function (err, num) {
+                names[num] = name;
+                socket.emit('ready', num);
+            });
+            broadcast('names', names);
+        });
+    });
+
+    socket.on('size', function (size) {
+        height = size.height > height ? height : size.height;
+        width = size.width > width ? width : size.width;
+
+        if (width < 900) width = 900;
+        if (height < 600) height = 600;
+
+        broadcast('size', {
+            width: width,
+            height: height
+        });
+    });
+
+    socket.on('disconnect', function () {
+        socket.get('name', function (err, name) {
+            broadcast('disconnect', name);
+        });
+    });
+
+    socket.on('died', function(){
+        socket.get('num', function (err, num){
+            console.log('remove ' + num);
+            broadcast('remove', num);
+        });
+    });
+
+    socket.on('position', function (pos) {
+        socket.get('num', function (err, num) {
+            positions[num] = pos;
+            if (pos.bulletPosX !== -1 && pos.bulletPosY !== -1) {
+                for (var k = 0; k < positions.length; k++) {
+                    if (k == num) continue;
+                    var x = pos.bulletPosX;
+                    var y = pos.bulletPosY;
+                    if (Math.sqrt(Math.pow((positions[k].shipX - x), 2) + Math.pow((positions[k].shipY - y), 2)) <= 30) {
+                        socket.emit('stopBullet');
+                        clients[k].emit('hit');
+                        positions[num].bulletPosX = -1;
+                        positions[num].bulletPosY = -1;
+                    }
+                }
+            }
+        });
+    });
+});
+
+setInterval(function () {
+    broadcast('positions', positions);
+}, 150);
+
+var broadcast = function (event, data) {
+    clients.forEach(function (socket) {
+        socket.emit(event, data);
+    });
+};
+
+server.listen(process.env.PORT || 8000, process.env.IP || "0.0.0.0", function () {
+    var addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port);
+});
